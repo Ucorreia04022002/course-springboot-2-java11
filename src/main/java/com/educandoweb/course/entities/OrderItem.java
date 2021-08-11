8fb3fd28e0ef36547cfbf1e0c445f77ac1a8e40d
ulisses040202 <ulissescoquettocorreia@gmail.com>
2021-08-11 00:52:34 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -34,27 +34,35 @@ public class OrderItem implements Serializable {
 	public Order getOrder() {
 		return id.getOrder();
 	}
+	
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
+	
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
+	
 	public Integer getQuantity() {
 		return quantity;
 	}
+	
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+	
 	public Double getPrice() {
 		return price;
 	}
+	
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -62,6 +70,7 @@ public class OrderItem implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
